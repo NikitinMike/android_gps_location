132256c1405ecb11208e1a284e8d5159b6dca117
mike <jobles11@gmail.com>
2021-09-03 15:54:06 +0300

    init

---
+++ app/src/main/java/com/example/mymapsx/DBHelper.java
@@ -0,0 +1,116 @@
+package com.example.mymapsx;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+class DBHelper extends SQLiteOpenHelper {
+
+    final String LOG_TAG = "myDBLogs";
+    final String mytable = "mytable2";
+    SQLiteDatabase db;
+
+    boolean getDBRecord(double latitude,double longitude){
+        if(db.isOpen()){
+            Cursor c = db.rawQuery("SELECT id,datetime FROM "+mytable+" WHERE "
+                    +"latitude = "+latitude+" AND " +"longitude = "+longitude, null);
+            if (c.moveToFirst()){
+                do {
+                    // Passing values
+                    long id = c.getLong(0);
+                    long time = c.getLong(1);
+                    // Do something Here with values
+                } while(c.moveToNext());
+                return true;
+            }
+            c.close();
+        }
+        return false;
+    }
+
+    long saveDBRecord(long time,double latitude,double longitude) {
+        if(!db.isOpen())return 0;
+        if(getDBRecord(latitude,longitude)) return 0;
+        // создаем объект для данных
+        ContentValues cv = new ContentValues();
+//        cv.put("name", "name");
+//        cv.put("email", "email");
+        cv.put("datetime", time);
+        cv.put("latitude", latitude);
+        cv.put("longitude", longitude);
+        // вставляем запись и получаем ее ID
+        long rowID = db.insert(mytable, null, cv);
+        Log.d(LOG_TAG, "row inserted, ID = " + rowID+" "+cv);
+        return rowID;
+    }
+
+    public DBHelper(Context context,String name) {
+        // конструктор суперкласса
+        super(context, name, null, 1);
+        // подключаемся к БД
+        db = getWritableDatabase();
+//        clearDBData();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(LOG_TAG, "--- onCreate database ---");
+        db.execSQL("DROP TABLE IF EXISTS "+mytable);
+        // создаем таблицу с полями
+        db.execSQL("CREATE TABLE "+mytable+" ("
+                + "id integer primary key autoincrement,"
+                + "datetime integer,"
+                + "latitude real,"
+                + "longitude real"
+                + ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+mytable);
+        onCreate(db);
+    }
+
+    void clearDBData() {
+        Log.d(LOG_TAG, "--- Clear mytable: ---");
+        // удаляем все записи
+        int clearCount = db.delete(mytable, null, null);
+        Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+    }
+
+    void getDBData() {
+        // делаем запрос всех данных из таблицы mytable, получаем Cursor
+        Cursor c = db.query(mytable, null, null, null, null, null, null);
+        // ставим позицию курсора на первую строку выборки
+        // если в выборке нет строк, вернется false
+        Log.d(LOG_TAG,"----------------------------------------------------------------");
+        if (c.moveToFirst()) {
+            // определяем номера столбцов по имени в выборке
+            int idColIndex = c.getColumnIndex("id");
+            int datetimeColIndex = c.getColumnIndex("datetime");
+            int latitudeColIndex = c.getColumnIndex("latitude");
+            int longitudeColIndex = c.getColumnIndex("longitude");
+//            int nameColIndex = c.getColumnIndex("name");
+//            int emailColIndex = c.getColumnIndex("email");
+            do {
+                // получаем значения по номерам столбцов и пишем все в лог
+                Log.d(LOG_TAG,
+                        "ID = " + c.getInt(idColIndex)
+                                +", time = " + c.getString(datetimeColIndex)
+                                +", latitude = " + c.getString(latitudeColIndex)
+                                +", longitude = " + c.getString(longitudeColIndex)
+//                        ", name = " + c.getString(nameColIndex) +
+//                        ", email = " + c.getString(emailColIndex)
+                );
+                // переход на следующую строку
+                // а если следующей нет (текущая - последняя), то false - выходим из цикла
+            } while (c.moveToNext());
+        } else
+            Log.d(LOG_TAG, "0 rows");
+        c.close();
+    }
+
+}
---
+++ app/src/main/java/com/example/mymapsx/MapsActivity.java
@@ -0,0 +1,231 @@
+package com.example.mymapsx;
+
+import static java.util.Collections.*;
+
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.mymapsx.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    final String LOG_TAG = "myLogs";
+    static final ArrayList<String> locations = new ArrayList<>();
+    static final ArrayList<LatLng> locationPoints = new ArrayList<>();
+    int points = 1000;
+    int scale = 10000000;
+    int PRECISION = 1;
+    long startDateTime = 0;
+    long skipCounter = 0;
+    public static final String PREFS_NAME = "MyMapPrefs";
+    public static final String PREFS_LOC = "locations";
+    public static final String myDB = "myMapsDB";
+    int n = 0;
+//    SupportMapFragment mapFragment;
+    LatLng base;
+
+    static Window window;
+    static Timer timer;
+//    DBHelper dbHelper;
+
+    @Override
+    protected void onResume() {
+        restorePrefs();
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        savePrefs();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.setTitle(MyLocationListener.getLocation());
+//        freshListView();
+//        dbHelper.getDBData();
+    }
+
+    void restorePrefs(){
+        //restore preferences
+        SharedPreferences settings = this.getSharedPreferences(PREFS_NAME, 0);
+        locations.addAll(settings.getStringSet(PREFS_LOC, new HashSet(locations)));
+//        points = locations.size(); System.out.println(points);
+//        Log.d(LOG_TAG,"====================================================================");
+//        System.out.println(locations);
+        for (String s : locations) locationPoints.add(getPoint(s));
+    }
+
+    void savePrefs(){
+//        Log.d(LOG_TAG,"******************************************************************");
+//        System.out.println(locations);
+        SharedPreferences.Editor editor = this.getSharedPreferences(PREFS_NAME, 0).edit();
+        editor.putStringSet(PREFS_LOC, new HashSet(locations));
+        editor.apply();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        MyLocationListener.SetUpLocationListener(this);
+
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        startDateTime = new Date().getTime();
+
+//        dbHelper = new DBHelper(this,myDB);
+//        dbHelper.clearDBData();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        restorePrefs();
+        points = locationPoints.size();
+        locationPoints.sort(this::compare);
+
+        Location location = MyLocationListener.imHere;
+        base = new LatLng(location.getLatitude(), location.getLongitude());
+        Log.i(LOG_TAG, base.toString());
+
+        // выполняем задачу MyTimerTask, описание которой будет ниже:
+        window = this.getWindow();
+        timer = new Timer();
+        timer.schedule(new MyTimerTask(), 10000, 10000);
+    }
+
+    int compare(LatLng b, LatLng a) {
+        return (int) (((a.latitude == b.latitude) ? a.longitude - b.longitude : a.latitude - b.latitude) * scale);
+//        return (int) (((a.longitude == b.longitude) ?a.latitude-b.latitude:a.longitude-b.longitude)*scale);
+//        return (int) (Math.abs(a.longitude-b.longitude) + Math.abs(a.latitude-b.latitude)*1000000);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 11.0f));
+        refreshMap();
+    }
+
+//    private void freshListView() {
+//        ListView positions = findViewById(R.id.coordinatesList);
+//        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, locations.toArray());
+//        // используем адаптер данных
+//        positions.setAdapter(adapter);
+//    }
+
+    void refreshMap() {
+        if (skipCounter == 0) {
+            //        PolylineOptions line = new PolylineOptions();
+            MarkerOptions markerOptions = setMarker(10);
+            //        line.width(4f).color(R.color.indigo_900);
+            //        LatLngBounds.Builder latLngBuilder = new LatLngBounds.Builder();
+            for (LatLng point : locationPoints) {
+                //            if(point!=null) line.add(point);
+                if (point != null) {
+                    markerOptions.position(point);
+                    mMap.addMarker(markerOptions);
+                }
+            }
+        }
+        Location location = MyLocationListener.imHere;
+        LatLng me = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(me));
+//        10: City
+//        15: Streets
+//        20: Buildings
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(me, 17.0f));
+        mMap.addMarker(new MarkerOptions().position(me).title("It's Me"));
+    }
+
+    LatLng getPoint(String o) {
+        final Pattern pattern = Pattern.compile("(\\d+,\\d+);(\\d+,\\d+)");
+        Matcher matcher = pattern.matcher(o);
+        if (matcher.find()) {
+            return new LatLng(
+                Float.parseFloat(Objects.requireNonNull(matcher.group(1)).replace(",", ".")),
+                Float.parseFloat(Objects.requireNonNull(matcher.group(2)).replace(",", "."))
+            );
+        }
+        return null;
+    }
+
+    MarkerOptions setMarker(int bits) {
+        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.alpha(0.1F);
+        Bitmap bitmap = Bitmap.createBitmap(bits, bits, Bitmap.Config.ARGB_8888);
+        for (int x = 0; x < bits; x++)
+            for (int y = 0; y < bits; y++) bitmap.setPixel(x, y, Color.BLACK);
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(bitmap));
+//        markerOptions.icon( BitmapDescriptorFactory.fromAsset("pixel.bmp") );
+//                Bitmap.createBitmap(1,1,new Bitmap.Config()));
+        return markerOptions;
+    }
+
+    // Метод для описания того, что будет происходить при работе таймера (задача для таймера):
+    class MyTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            // Отображаем информацию в текстовом поле count:
+            runOnUiThread(() -> {
+                window.setTitle(new Date().toString());
+                String text = MyLocationListener.getLocation();
+                locations.add(text);
+                LatLng p = getPoint(text);
+                locationPoints.add(p);
+                Log.i(LOG_TAG, p.toString()+":"+locationPoints.size());
+//                locationPoints.forEach(lp->Log.i(LOG_TAG, lp.toString()));
+//                dbHelper.saveDBRecord(new Date().getTime(),p.latitude*PRECISION,p.longitude*PRECISION);
+//                System.out.println((new Date().getTime()-start)/1000+" "+locationPoints.size()+" "+text);
+//                freshListView();
+                if ((++skipCounter % 10) == 0) refreshMap();
+            });
+        }
+    }
+}
